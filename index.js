803c6dd08d5a19ce2c3e1294f58a2e7d3a00970d
Samuel Fisher <skiy23@outlook.com>
2022-01-20 18:05:52 -0800

    complete
    break

--- index.js
+++ index.js
@@ -12,43 +12,21 @@ move(newItem('assets/staff.png')).to(600, 250)
 
 const character = newImage('assets/green-character/static.gif')
 
-let direction = null;
-let x = 100;
-let y = 250;
-
-function moveCharacter(){ 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = 'assets/green-character/west.gif'
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = 'assets/green-character/north.gif'
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = 'assets/green-character/east.gif'
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = 'assets/green-character/south.gif'
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
 }
-setInterval(moveCharacter,1)
-
-document.addEventListener('keydown', function(e){     
-    if(e.repeat) return           
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-move(character).to(100, 250)
-
-
+move(character).withArrowKeys(100, 250, handleDirectionChange)
